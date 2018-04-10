f16fff8297498ea041c2bb61828c0196bee25ed8
alessioRM87 <alessio.iannella@gmail.com>
2018-04-09 21:38:55 -0400

    // CREATE EVENT
    Create event with photo and category

--- app/src/main/java/com/example/yun/meetup/activities/CreateEventActivity.java
+++ app/src/main/java/com/example/yun/meetup/activities/CreateEventActivity.java
@@ -1,14 +1,26 @@
 package com.example.yun.meetup.activities;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -24,6 +36,8 @@ import com.example.yun.meetup.models.Event;
 import com.example.yun.meetup.models.UserInfo;
 import com.example.yun.meetup.requests.CreateEventRequest;
 
+import java.io.File;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -44,8 +58,18 @@ public class CreateEventActivity extends AppCompatActivity {
     private TextView textViewErrorCreateEventTitle;
     private TextView textViewErrorCreateEventDate;
     private TextView textViewErrorCreateEventAddress;
+    private TextView textViewErrorCreateEventCategory;
     private TextView textViewErrorCreateEvent;
     private Spinner spinnerCategory;
+
+    private static final int REQUEST_PERMISSIONS = 2;
+    private static final String[] PERMISSIONS_TO_REQUEST = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    private static final int PICKIMAGE_REQUESTCODE = 1;
+
+    private File mFile;
+    private Event mEvent;
+    private String mCategory = "";
+    private List<String> mCategoryList;
 
 
     Calendar calendar = Calendar.getInstance();
@@ -74,17 +98,32 @@ public class CreateEventActivity extends AppCompatActivity {
         editTextCreateEventAddress = (EditText) findViewById(R.id.editTextCreateEventAddress);
         textViewErrorCreateEventAddress = (TextView) findViewById(R.id.textViewErrorCreateEventAddress);
 
+        textViewErrorCreateEventCategory = (TextView) findViewById(R.id.textViewErrorCreateEventCategory);
+
         editTextCreateEventDescription = (EditText) findViewById(R.id.editTextCreateEventDescription);
 
         textViewErrorCreateEvent = (TextView) findViewById(R.id.textViewErrorCreateEvent);
 
         spinnerCategory = findViewById(R.id.spinner_category_create_event);
 
-//        String[] categoryArray = getResources().getStringArray(R.array.category_array);
-//        List<String> categoryList = Arrays.asList(categoryArray);
-//
-//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_item, categoryList);
+        String[] categoryArray = getResources().getStringArray(R.array.category_array);
+        mCategoryList = Arrays.asList(categoryArray);
+
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_item, mCategoryList);
 //        spinnerCategory.setAdapter(adapter);
+
+        spinnerCategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mCategory = mCategoryList.get(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+
 
 
     }
@@ -134,6 +173,16 @@ public class CreateEventActivity extends AppCompatActivity {
             textViewErrorCreateEventAddress.setVisibility(View.VISIBLE);
             error = true;
         }
+        if (editTextCreateEventAddress.getText().toString().isEmpty()) {
+            textViewErrorCreateEventAddress.setText("Please provide an Address for your event!");
+            textViewErrorCreateEventAddress.setVisibility(View.VISIBLE);
+            error = true;
+        }
+        if (mCategory == "" || mCategory.isEmpty()){
+            textViewErrorCreateEventCategory.setText("Please select a category for your event!");
+            textViewErrorCreateEventCategory.setVisibility(View.VISIBLE);
+            error = true;
+        }
 
         if (!error) {
             CreateEventRequest createEventRequest = new CreateEventRequest();
@@ -159,6 +208,46 @@ public class CreateEventActivity extends AppCompatActivity {
         textViewErrorCreateEventTitle.setVisibility(View.GONE);
         constraintLayoutLoading.setVisibility(View.GONE);
         textViewErrorCreateEvent.setVisibility(View.GONE);
+        textViewErrorCreateEventCategory.setVisibility(View.GONE);
+    }
+
+    public void handleOnClickEventPhoto(View view) {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, PERMISSIONS_TO_REQUEST, REQUEST_PERMISSIONS);
+
+        } else {
+            Intent pickImageIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(pickImageIntent, PICKIMAGE_REQUESTCODE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICKIMAGE_REQUESTCODE) {
+            if (resultCode == RESULT_OK) {
+
+                Uri tempUri = data.getData();
+
+                mFile = new File(getRealPathFromURI(tempUri));
+            }
+        }
+        else if (requestCode == REQUEST_PERMISSIONS){
+            if (resultCode == RESULT_OK){
+                Intent pickImageIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(pickImageIntent, PICKIMAGE_REQUESTCODE);
+            }
+        }
+    }
+
+    private String getRealPathFromURI(Uri uri) {
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+        return cursor.getString(idx);
     }
 
     private class ValidateAddressTask extends AsyncTask<CreateEventRequest, Void, APIResult> {
@@ -199,10 +288,55 @@ public class CreateEventActivity extends AppCompatActivity {
 
             if (result.isResultSuccess()) {
                 Toast.makeText(CreateEventActivity.this, "Event created successfully!", Toast.LENGTH_SHORT).show();
-                CreateEventActivity.this.finish();
+                mEvent = (Event) result.getResultEntity();
+                new UploadPhotoTask().execute(mFile);
+
             } else {
                 textViewErrorCreateEvent.setText(result != null ? result.getResultMessage() : "Please contact admin staff!");
                 textViewErrorCreateEvent.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    private class UploadPhotoTask extends AsyncTask<File, Void, APIResult>{
+
+        @Override
+        protected APIResult doInBackground(File...files) {
+            NetworkManager networkManager = new NetworkManager();
+            return networkManager.uploadEventPhoto(files[0], mEvent.get_id());
+        }
+
+        @Override
+        protected void onPostExecute(APIResult apiResult) {
+
+            if (!apiResult.isResultSuccess()){
+                Toast.makeText(CreateEventActivity.this, apiResult.getResultMessage(), Toast.LENGTH_SHORT).show();
+
+                new DeleteEventTask().execute(mEvent.get_id());
+            }
+            else{
+                hideViews();
+                CreateEventActivity.this.finish();
+            }
+        }
+    }
+
+    private class DeleteEventTask extends AsyncTask<String, Void, APIResult>{
+
+        @Override
+        protected APIResult doInBackground(String... strings) {
+            NetworkManager networkManager = new NetworkManager();
+            return networkManager.deleteEvent(strings[0]);
+        }
+
+        @Override
+        protected void onPostExecute(APIResult apiResult) {
+            if (!apiResult.isResultSuccess()){
+                Toast.makeText(CreateEventActivity.this, apiResult.getResultMessage(), Toast.LENGTH_SHORT).show();
+                new DeleteEventTask().execute(mEvent.get_id());
+            }
+            else{
+                hideViews();
             }
         }
     }
